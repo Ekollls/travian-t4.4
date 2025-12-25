10d9a263cd639e0abfa87331a6ba3d1f6c679a7f
Ekollls <ekoll5959@gmail.com>
2025-12-25 02:28:40 -0800

    travian-t4.4
    travian-t4.4 php script full

---
+++ action-panel/navbar.php
@@ -0,0 +1,30 @@
+<div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar">
+			
+				<div class="custom-menu">
+				
+					<button type="button" id="sidebarCollapse" class="btn btn-primary">
+					
+	          <i class="fa fa-bars"></i>
+			  
+	          <span class="sr-only">Toggle Menu</span>
+			  
+	        </button>
+        </div><br>
+				<div class="p-4 pt-5"> 
+         <ul class="list-unstyled components mb-5">
+          <li><a href="index.php"><?php echo $lang['Admin']['sidebar01']; ?></a></li>
+<li><a href="?p=acikartirma">Auction</a></li> <li><a href="?p=acikartirmav1">Auction v1</a></li> <li><a href="?p=acikartirmatopluV1">Bulk Auction V1</a></li> <li><a href="?p=acikartirmatopluV2">Bulk Auction V2</a></li> <li><a href="?p=kafes">Add Auction Cage</a></li> <li><a href="?p=depodoldur">Fill Warehouses</a></li> <li><a href="?p=vahadoldur">Fill Oases</a></li>
+	          <li>
+	           
+            </li>
+	          <li>
+
+
+	        <div class="footer">
+            <?php $isOkay = TRUE; ?>
+	        	<p><?php echo $lang['Admin']['sidebar29']; ?> <a style="color:white;" href="#"></a>.</p>
+	        </div>
+
+	      </div>
+    	</nav>
---
+++ application/views/footer.php
@@ -0,0 +1,18 @@
+<div class="clear"></div>
+				</div>
+<div id="footer">
+            <!--email_off-->
+            <div id="pageLinks">
+                <a href="#" target="_blank"><?=$lang['Footer']['Homepage'];?></a>
+                <a href="#" target="_blank"><?=$lang['Footer']['Forum'];?></a>
+                <a href="#" target="_blank"><?=$lang['Footer']['FAQ'];?></a>
+                <a href="#" target="_blank"><?=$lang['Footer']['Terms'];?></a>
+                <a href="#" target="_blank"><?=$lang['Footer']['Imprint'];?></a>
+                <div class="clear"></div>
+            </div>
+            <br>
+            <p class="copyright" style="direction:ltr;">© 2019 - 2020 Travian Games GmbH</p>
+			                                 
+            <!--/email_off-->
+            <br>
+        </div>
